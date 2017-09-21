df11fc8bc6deacb1a595d2584addfeb7dd228cb9
TheYoungSensei <sacre.christopher@hotmail.com>
2017-09-21 23:24:02 +0200

    Adding Struct + TSORT.java

---
+++ I310B_Res_problemes/TSORT.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+ 
+public class Main {
+	
+	public static java.util.Scanner scanner = new java.util.Scanner(System.in);
+ 
+	public static void main(String[] args) {
+		int nbLignes;
+		nbLignes = scanner.nextInt();
+		int elems[] = new int[1000000];
+		for(int i = 0; i < nbLignes; i++) {
+			int elem = scanner.nextInt();
+			elems[i] = elem;
+		}
+		Arrays.sort(elems);
+		for(int i = 0; i < elems.length; i++) {	
+				System.out.println(elems[i]);
+		}
+		
+	}
+} 
