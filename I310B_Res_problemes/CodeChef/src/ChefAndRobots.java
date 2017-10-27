671131a9ae10e9ebff28ad10c7b918e9ae0b80a1
TheYoungSensei <sacre.christopher@hotmail.com>
2017-10-27 08:37:26 +0200

    Commit Pre .NET

--- I310B_Res_problemes/CodeChef/src/ChefAndRobots.java
+++ I310B_Res_problemes/CodeChef/src/ChefAndRobots.java
@@ -1,12 +1,23 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
+/*
+2
+4 4 1 1
+0 1 1 0
+0 1 1 0
+0 1 1 0
+0 0 0 0
+4 4 1 1
+0 1 1 0
+0 1 1 0
+0 1 1 0
+1 0 0 1
+ */
+
+import java.util.*;
 
 public class ChefAndRobots {
 
     private static java.util.Scanner scanner = new java.util.Scanner(System.in);
-    private static boolean found;
+    private static Case found;
 
     private static class Case {
         int ligne;
@@ -36,16 +47,22 @@ public class ChefAndRobots {
     }
 
     private static int map[][];
+    private static int mapRobot1[][][];
+    private static int mapRobot2[][][];
     private static int nbMovesRobot2;
     private static int nbMovesRobot1;
     private static Case robot1;
     private static Case robot2;
     private static Set<Case> rfrobot1;
     private static Set<Case> rfrobot2;
-    private static Deque<Case> frobot1;
-    private static Deque<Case> frobot2;
-    private static int nbIte;
-
+    private static Case frobot1[];
+    private static int szfrobot1;
+    private static int szfrobot2;
+    private static int teteRobot1;
+    private static int teteRobot2;
+    private static Case frobot2[];
+    private static final int ROBOT1 = 1;
+    private static final int ROBOT2 = 2;
 
     public static void main(String[] args) {
         int testCases = scanner.nextInt();
@@ -65,6 +82,8 @@ public class ChefAndRobots {
         //K2
         nbMovesRobot2 = scanner.nextInt();
         map = new int[nbRows][nbColumns];
+        mapRobot1 = new int[nbRows][nbColumns][1];
+        mapRobot2 = new int[nbRows][nbColumns][1];
         for(int i = 0; i < nbRows; i++) {
             for(int j = 0; j < nbColumns; j++) {
                 map[i][j] = scanner.nextInt();
@@ -74,95 +93,117 @@ public class ChefAndRobots {
         robot2 = new Case(0, nbColumns - 1);
         rfrobot1 = new HashSet<>();
         rfrobot2 = new HashSet<>();
-        frobot1 = new ArrayDeque<>();
-        frobot2 = new ArrayDeque<>();
+        frobot1 = new Case[nbRows * nbColumns];
+        szfrobot1 = 0;
+        teteRobot1 = 0;
+        teteRobot2 = 0;
+        frobot2 = new Case[nbRows * nbColumns];
+        szfrobot2 = 0;
+    }
+
+    private static void addLast(Case c, int robot) {
+        if(robot == 1) {
+            frobot1[teteRobot1] = c;
+            szfrobot1++;
+        } else {
+            frobot2[teteRobot2] = c;
+            szfrobot2++;
+        }
+    }
+
+    private static Case removeFirst(int robot) {
+        Case c = null;
+        if(robot == 1) {
+            c = frobot1[teteRobot1];
+            teteRobot1++;
+            szfrobot1--;
+        } else {
+            c = frobot2[teteRobot2];
+            teteRobot2++;
+            szfrobot2--;
+        }
+        return c;
     }
 
     private static void initialisation() {
         //Initialisation Robot 1
         rfrobot1.add(robot1);
-        frobot1.add(robot1);
+        addLast(robot1, ROBOT1);
         //Initialisation Robot 2
         rfrobot2.add(robot2);
-        frobot2.add(robot2);
-        found = false;
-        nbIte = 0;
-        while(!frobot1.isEmpty() && !frobot2.isEmpty()) {
-            Case cRobot1 = frobot1.pop();
-            Case cRobot2 = frobot2.pop();
-            succ1(cRobot1);
-            succ2(cRobot2);
+        addLast(robot2, ROBOT2);
+        found = null;
+        while(szfrobot1 != 0 && szfrobot2 != 0 && found == null) {
+            Case cRobot1 = removeFirst(ROBOT1);
+            Case cRobot2 = removeFirst(ROBOT2);
+            succ(cRobot1, ROBOT1);
+            succ(cRobot2, ROBOT2);
         }
-        if(found) {
-            System.out.println(nbIte);
+        if(found != null) {
+            if(mapRobot1[found.ligne][found.colonne][0] < mapRobot2[found.ligne][found.colonne][0]) {
+                System.out.println(mapRobot2[found.ligne][found.colonne][0]);
+            } else {
+                System.out.println(mapRobot1[found.ligne][found.colonne][0]);
+            }
         } else {
             System.out.println(-1);
         }
     }
 
-    private static void succ1(Case c) {
-        chain1(0, c);
+    private static void succ(Case c, int robot) {
+        chain(0, c, c, robot);
     }
 
-    private static void chain1(int deplacements, Case c) {
-        if(!rfrobot1.contains(c) && map[c.ligne][c.colonne] != 1) {
-            rfrobot1.add(c);
-            frobot1.add(c);
+    private static void chain(int deplacements, Case c, Case parent, int robot) {
+        if(robot == ROBOT1) {
+            if(!rfrobot1.contains(c) && map[c.ligne][c.colonne] != 1) {
+                rfrobot1.add(c);
+                addLast(c, ROBOT1);
+                if(!c.equals(parent)) {
+                    if(mapRobot1[parent.ligne][parent.colonne][0] + 1 <  mapRobot1[c.ligne][c.colonne][0] || mapRobot1[c.ligne][c.colonne][0] == 0) {
+                        mapRobot1[c.ligne][c.colonne][0] = mapRobot1[parent.ligne][parent.colonne][0] + 1;
+                    }
+                }
+            }
+            if(rfrobot2.contains(c)) {
+                found = c;
+            }
+            if(deplacements >= nbMovesRobot1) {
+                return;
+            }
+        } else {
+            if(!rfrobot2.contains(c) && map[c.ligne][c.colonne] != 1) {
+                rfrobot2.add(c);
+                addLast(c, ROBOT2);
+            }
+            if(!c.equals(parent)) {
+                if(mapRobot2[parent.ligne][parent.colonne][0] + 1 <  mapRobot2[c.ligne][c.colonne][0] || mapRobot2[c.ligne][c.colonne][0] == 0) {
+                    mapRobot2[c.ligne][c.colonne][0] = mapRobot2[parent.ligne][parent.colonne][0] + 1;
+                }
+            }
+            if(rfrobot1.contains(c)) {
+                found = c;
+            }
+            if(deplacements >= nbMovesRobot2) {
+                return;
+            }
         }
-        if(deplacements < nbMovesRobot1) {
-            //Haut
-            if(c.ligne < map.length - 1) {
-                chain1(deplacements + 1, new Case(c.ligne + 1, c.colonne));
-            }
-            //Bas
-            if(c.ligne != 0) {
-                chain1(deplacements + 1, new Case(c.ligne - 1, c.colonne));
-            }
-            //Gauche
-            if(c.colonne != 0) {
-                chain1(deplacements + 1, new Case(c.ligne, c.colonne - 1));
-            }
-            //Droite
-            if(c.colonne < map[c.ligne].length - 1) {
-                chain1(deplacements + 1, new Case(c.ligne, c.colonne + 1));
-            }
+        //Haut
+        if(c.ligne < map.length - 1) {
+            chain(deplacements + 1, new Case(c.ligne + 1, c.colonne), parent, robot);
         }
-    }
-
-    private static void succ2(Case c) {
-        if(!found)
-            nbIte += 1;
-        chain2(0, c);
-    }
-
-    private static void chain2(int deplacements, Case c) {
-        if(!rfrobot2.contains(c) && map[c.ligne][c.colonne] != 1) {
-            rfrobot2.add(c);
-            frobot2.add(c);
+        //Bas
+        if(c.ligne != 0) {
+            chain(deplacements + 1, new Case(c.ligne - 1, c.colonne), parent, robot);
         }
-        if(rfrobot1.contains(c)) {
-            found = true;
+        //Gauche
+        if(c.colonne != 0) {
+            chain(deplacements + 1, new Case(c.ligne, c.colonne - 1), parent, robot);
         }
-        if(deplacements < nbMovesRobot2) {
-            //Haut
-            if(c.ligne < map.length - 1) {
-                chain2(deplacements + 1, new Case(c.ligne + 1, c.colonne));
-            }
-            //Bas
-            if(c.ligne != 0) {
-                chain2(deplacements + 1, new Case(c.ligne - 1, c.colonne));
-            }
-            //Gauche
-            if(c.colonne != 0) {
-                chain2(deplacements + 1, new Case(c.ligne, c.colonne - 1));
-            }
-            //Droite
-            if(c.colonne < map[c.ligne].length - 1) {
-                chain2(deplacements + 1, new Case(c.ligne, c.colonne + 1));
-            }
+        //Droite
+        if(c.colonne < map[c.ligne].length - 1) {
+            chain(deplacements + 1, new Case(c.ligne, c.colonne + 1), parent, robot);
         }
     }
-
-
-
 }
+
