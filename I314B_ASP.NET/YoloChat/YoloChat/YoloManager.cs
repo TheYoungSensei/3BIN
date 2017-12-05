46123e7036bdd3540affcd7201f9388bf57e8871
TheYoungSensei <sacre.christopher@hotmail.com>
2017-12-05 03:03:26 +0100

    Implementing Yolo Chat

---
+++ I314B_ASP.NET/YoloChat/YoloChat/YoloManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoloChat
+{
+    public class YoloManager
+    {
+        public List<dynamic> Messages { get; set; } = new List<dynamic>();
+    }
+}
