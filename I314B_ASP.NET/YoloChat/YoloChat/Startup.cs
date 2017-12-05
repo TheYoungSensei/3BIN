da9866f3dd763297d0f01deb1392af587b587726
TheYoungSensei <sacre.christopher@hotmail.com>
2017-12-05 03:09:05 +0100

    Implemented YoloChat

---
+++ I314B_ASP.NET/YoloChat/YoloChat/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using YoloChat.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebSocketManager;
+
+namespace YoloChat
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebSocketManager();
+            services.AddSingleton<YoloManager>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseWebSockets();
+            app.MapWebSocketManager("/chat", serviceProvider.GetService<YoloHandler>());
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ I314B_ASP.NET/YoloChat/YoloChat/YoloHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSocketManager;
+
+namespace YoloChat
+{
+    public class YoloHandler : WebSocketHandler
+    {
+
+        private readonly YoloManager _yoloManager;
+        public YoloHandler(WebSocketConnectionManager webSocketConnectionManager, YoloManager yoloManager) : base(webSocketConnectionManager)
+        {
+            _yoloManager = yoloManager;
+        }
+
+        public async Task SendMessage(string socketId, string message)
+        {
+            dynamic dynamicMessage = new ExpandoObject();
+            dynamicMessage.UserId = socketId;
+            dynamicMessage.Message = message;
+            _yoloManager.Messages.Add(dynamicMessage);
+            await InvokeClientMethodToAllAsync("pingMessage", socketId, message);
+        }
+    }
+}
